a2d8db03b5b2194d6dc052a5ee132eff654710a8
Rafael Sánchez-Piñol <146205862+rafaspy@users.noreply.github.com>
2026-01-14 21:18:46 +0100

    Add files via upload
    empiezo

---
+++ libft.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+int ft_isalnum(int c);
+
+int ft_isascii(int c)
+{
+}
+
+int main(void)
+{
+	printf("Mi funcion devuelve %d", ft_isdigit('f'));
+	return (0);
+}
