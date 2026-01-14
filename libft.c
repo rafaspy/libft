1f6c763d19f8ad16b721572c0327c832ccd10c3e
Rafael Sánchez-Piñol <sanchez.rafael19@gmail.com>
2026-01-14 21:33:21 +0100

    nose

--- libft.c
+++ libft.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int ft_isdigit(int c)
@@ -19,26 +18,3 @@ int main(void)
 	printf("Mi funcion devuelve %d", ft_isdigit('f'));
 	return (0);
 }
-=======
-#include <stdio.h>
-
-int ft_isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-
-	return (0);
-}
-int ft_isalnum(int c);
-// lo hare en 42
-
-int ft_isascii(int c)
-{
-}
-
-int main(void)
-{
-	printf("Mi funcion devuelve %d", ft_isdigit('f'));
-	return (0);
-}
->>>>>>> 67b87a7 (empiezo el proyecto)
